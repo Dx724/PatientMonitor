9ebf222244eda2ff0d61c9a6082736269f1f4f34
tzussman <talzuss@gmail.com>
2020-05-19 20:51:17 -0400

    Updates

---
+++ precache-manifest.2a6ea8133e314b2b89d8699744ec60ef.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad400e91d443b473cdbb97f8914aef8",
+    "url": "/PatientMonitor/index.html"
+  },
+  {
+    "revision": "6ee1d9f4aea3014f20c7",
+    "url": "/PatientMonitor/static/css/main.ed619b46.chunk.css"
+  },
+  {
+    "revision": "1ecdf5a6f1455dd64f5d",
+    "url": "/PatientMonitor/static/js/2.6d6d14da.chunk.js"
+  },
+  {
+    "revision": "959cc738e4b00af4846a5252908373f7",
+    "url": "/PatientMonitor/static/js/2.6d6d14da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee1d9f4aea3014f20c7",
+    "url": "/PatientMonitor/static/js/main.a0f27533.chunk.js"
+  },
+  {
+    "revision": "0e538822c8d00f3ef44f",
+    "url": "/PatientMonitor/static/js/runtime-main.e1a9f678.js"
+  },
+  {
+    "revision": "0f48c6dfd09d3b3d55af6d7c098d22ab",
+    "url": "/PatientMonitor/static/media/Columbia_University_Logo-white.0f48c6df.png"
+  },
+  {
+    "revision": "56ecfc67855e255d92db6f6fcb97143e",
+    "url": "/PatientMonitor/static/media/roboto-v20-latin-ext_latin-300.56ecfc67.woff2"
+  },
+  {
+    "revision": "f4b08b5a857ef12352aaf0ebc1316460",
+    "url": "/PatientMonitor/static/media/roboto-v20-latin-ext_latin-300.f4b08b5a.woff"
+  },
+  {
+    "revision": "15431381890720a5a4b62b33c8ae06c5",
+    "url": "/PatientMonitor/static/media/roboto-v20-latin-ext_latin-500.15431381.woff2"
+  },
+  {
+    "revision": "3d9446f210892af971a3854d0f984ae9",
+    "url": "/PatientMonitor/static/media/roboto-v20-latin-ext_latin-500.3d9446f2.woff"
+  },
+  {
+    "revision": "52bb58c8cb04cf3eea4f9ac0afa1d1f6",
+    "url": "/PatientMonitor/static/media/roboto-v20-latin-ext_latin-700.52bb58c8.woff2"
+  },
+  {
+    "revision": "d0cc855e64aca1072711f49d1a38ca0c",
+    "url": "/PatientMonitor/static/media/roboto-v20-latin-ext_latin-700.d0cc855e.woff"
+  },
+  {
+    "revision": "c5bf51b68dc9fd7fe944d8947fe12518",
+    "url": "/PatientMonitor/static/media/roboto-v20-latin-ext_latin-regular.c5bf51b6.woff2"
+  },
+  {
+    "revision": "d679a90608a65cc9394f67448ca2094a",
+    "url": "/PatientMonitor/static/media/roboto-v20-latin-ext_latin-regular.d679a906.woff"
+  }
+]);
